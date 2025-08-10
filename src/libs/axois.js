3470b90b63393952b1a647517ec0b33fa5a31487
tushar981146 <tusharblackhathacker@gmail.com>
2025-08-10 19:56:36 +0530

    added env

--- src/libs/axois.js
+++ src/libs/axois.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: import.meta.env.HOSTPATH ? import.meta.env.HOSTPATH : "http://localhost:3000/api",
     withCredentials: true,
 });
 
